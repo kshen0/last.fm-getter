172bf1c75d079a0f0adf4cd33c521151449146e3
Kevin Shen <kwshen@gmail.com>
2013-01-15 03:27:55 -0500

    reads all entries

---
+++ createscript.py
@@ -0,0 +1,8 @@
+import sqlite3 as lite
+con = lite.connect("lastfm.db")
+
+with con:
+	cur = con.cursor()
+	cur.execute("DROP TABLE IF EXISTS Tracks")
+	cur.execute("CREATE TABLE Tracks(Title TEXT, Artist TEXT, Album TEXT, Date_text TEXT, Date_uts TEXT)")
+	print "Table 'Tracks' created"
--- getlastfm_tracks.py
+++ getlastfm_tracks.py
@@ -2,42 +2,67 @@ import sqlite3 as lite
 import urllib2
 import sys
 import json
+import time
+import os
+
+def appendRows(tracks, rows):
+	for song in tracks:
+		newRow = (song["name"], song["artist"]["#text"], song["album"]["#text"], song["date"]["#text"], song["date"]["uts"])
+		rows.append(newRow)
+	return rows
+
+def queryUrl(config):
+	url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=%s&page=%d&user=%s&to=%d&api_key=%s&format=json" %  (config["limit"], config["page"], config["username"], config["startTime"], config["apiKey"])
+	print url
+
+	# send request to API
+	response = urllib2.urlopen(url)
+	data = json.loads(response.read())
+	return data
+
+def insertRows(rows, con, rowNum):
+	with con:
+		cur = con.cursor()
+		cur.executemany("INSERT INTO Tracks VALUES(?, ?, ?, ?, ?)", rows)
+		print "Inserted rows %d-%d beginning with row %s" % (rowNum, rowNum + len(rows) - 1, json.dumps(rows[0]))
+	rowNum = rowNum + len(rows)
+	return rowNum
+
+os.system("createscript.py")
 
 # configuration
-username = "yellowJumpsuit"
-apiKey = "2be05c4b50dd8b0b315ca2181feb7b35"
-page = 1
-limit = 200
-url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=%s&page=%d&user=%s&api_key=%s&format=json" % (limit, page, username, apiKey)
+config = {
+	"startTime": int(time.time()) - 5, #data collection window from beginning of user history to 5 minutes ago
+	"username": "yellowJumpsuit",
+	"apiKey": "2be05c4b50dd8b0b315ca2181feb7b35",
+	"page": 1,
+	"limit": 200,
+}
 
-# send request to API
-response = urllib2.urlopen(url)
-data = json.loads(response.read())
+data = queryUrl(config)
+numPages = int(data["recenttracks"]["@attr"]["totalPages"])
 tracks = data["recenttracks"]["track"]
 
 # parse data to list of tuples
 rows = []
+rowNum = 1
 
+# special case: first page of results, which may include currently playing track
 # throw out currently playing track, if there is one
-tracks = tracks[1:]
+if "@attr" in tracks[0] and tracks[0]["@attr"]["nowplaying"] == "true":
+	tracks = tracks[1:]
 
-for song in tracks:
-	newRow = (song["name"], song["artist"]["#text"], song["album"]["#text"], song["date"]["#text"], song["date"]["uts"])
-	print newRow
-	rows.append(newRow)
-
+# open connection
 con = lite.connect("lastfm.db")
 
-with con:
-	cur = con.cursor()
-	cur.execute("DROP TABLE IF EXISTS Tracks")
-	cur.execute("CREATE TABLE Tracks(Title TEXT, Artist TEXT, Album TEXT, Date_text TEXT, Date_uts TEXT)")
-	cur.executemany("INSERT INTO Tracks VALUES(?, ?, ?, ?, ?)", rows)
+# append first page of song results
+appendRows(tracks, rows)
+rowNum = insertRows(rows, con, rowNum)
 
-	# retrieve data
-	'''
-	cur.execute("SELECT * FROM Tracks")
-	rows = cur.fetchall()
-	for row in rows:
-		print row
-	'''
+for pageNum in xrange(2, numPages + 1):
+	rows = []
+	config["page"] = pageNum
+	data = queryUrl(config)
+	tracks = data["recenttracks"]["track"]
+	appendRows(tracks, rows)
+	rowNum = insertRows(rows, con, rowNum)
